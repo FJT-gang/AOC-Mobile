e191205ed51d64f968d0649e3fc7c6934e418c30
ferre0160 <heydeferre@gmail.com>
2022-04-03 11:26:14 +0200

    Commit Ferre

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: Center(
+          child: Container(
+            child: Text('Hey'),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:aoc/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Home());
-}
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "AOC Mobile",
+      home: Home(),
+    ));
