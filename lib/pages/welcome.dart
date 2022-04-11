0a6328958ee7e3ef9b8865d37d7c0d6e880cc7b3
ShadowFlashy <jasdegreef@outlook.com>
2022-04-11 13:06:16 +0200

    Global color

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:aoc/pages/login.dart';
+import 'package:aoc/general/globals.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 207, 238, 255),
+      backgroundColor: Globals.bgLightBlue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -32,7 +33,7 @@ class _WelcomePageState extends State<WelcomePage> {
                 style: TextStyle(fontSize: 30),
               ),
               style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 53, 125, 166),
+                  primary: Globals.bgDarkBlue,
                   textStyle: TextStyle(color: Colors.white),
                   minimumSize: Size(150, 100)),
               onPressed: () {
@@ -46,7 +47,7 @@ class _WelcomePageState extends State<WelcomePage> {
                 style: TextStyle(fontSize: 30),
               ),
               style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 53, 125, 166),
+                  primary: Globals.bgDarkBlue,
                   textStyle: TextStyle(color: Colors.white),
                   minimumSize: Size(150, 100)),
               onPressed: () {
