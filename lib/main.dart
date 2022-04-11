4fadde00b0a4ecf885e3feb066961ca3f8ea60e6
ferre0160 <heydeferre@gmail.com>
2022-04-11 19:24:40 +0200

    HEB HET GEFIXT --FERRE

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,16 @@ import 'package:aoc/pages/welcome.dart';
 import 'package:aoc/pages/login.dart';
 import 'package:flutter/material.dart';
 
-
 // Google Fonts
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MaterialApp(
-    theme: ThemeData(
+      theme: ThemeData(
         textTheme: GoogleFonts.robotoTextTheme(),
       ),
       debugShowCheckedModeBanner: false,
       title: "AOC Mobile",
-      initialRoute: '/login',
+      initialRoute: '/calendar',
       routes: {
         '/login': (context) => LoginPage(),
         '/register': (context) => LoginPage(),
